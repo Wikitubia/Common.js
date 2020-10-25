6f47c2a48621ca8bf9aca31f04abbd1781fc3a15
Nintendofan885 <66736551+Nintendofan885@users.noreply.github.com>
2020-10-25 13:06:32 +0000

    Update

--- Common.js
+++ Common.js
@@ -1,28 +1,38 @@
 mw.loader.load('https://apis.google.com/js/platform.js');
 
-if(wgPageName == 'Special:Upload' || wgPageName == 'Special:MultipleUpload') {
-$('#wpUploadDescription').val('[[Category:Images]]');
+if (wgPageName === 'Special:Upload' || wgPageName === 'Special:MultipleUpload') {
+    $('#wpUploadDescription').val('[[Category:Images]]');
 }
 
 // change label
 $('.pi-data-label:contains("Username")').replaceWith('<h3 class="pi-data-label pi-secondary-font">Subscribers</h3>');
- 
-// UserBadges settings
+
 window.UserTagsJS = {
-	modules: {},
-	tags: {
-                bureaucrat: { link:'Project:Administration#Bureaucrats and Administrators' },
-		sysop: { link:'Project:Administration#Bureaucrats and Administrators' },
-		rollback: { link:'Project:Administration#Rollbacks and Chat Moderators' },
-                chatmoderator: { link:'Project:Administration#Rollbacks and Chat Moderators' }
-	}
+    modules: {},
+    tags: {
+        bureaucrat: { link: 'Project:Administration#Bureaucrats and Administrators' },
+        sysop: { link: 'Project:Administration#Bureaucrats and Administrators' },
+        rollback: { link: 'Project:Administration#Rollbacks and Chat Moderators' },
+        chatmoderator: { link: 'Project:Administration#Rollbacks and Chat Moderators' }
+    }
 };
 UserTagsJS.modules.inactive = 30;
 UserTagsJS.modules.mwGroups = ['bureaucrat', 'sysop', 'rollback', 'chatmoderator', 'bot'];
 UserTagsJS.modules.metafilter = { 'notautoconfirmed': ['newuser'] };
 UserTagsJS.modules.newuser = { days: 5, edits: 0 };
- 
-// LastEdited settings
+
+window.UserTagsJS = {
+    modules: {},
+    tags: {
+        discordfounder: { u: 'Discord Founder' },
+        discordadmin: { u: 'Discord Admin' },
+        discordmod: { u: 'Discord Moderator' },
+        verifiedyt: { u: 'Verified YouTuber' },
+        rollback: { u: 'Rollbacker' },
+        retired: { u: 'Retired Staff' }
+    }
+};
+
 window.lastEdited = {
     position: 'bottom',
     size: false,
@@ -31,190 +41,74 @@ window.lastEdited = {
 };
 
 window.railWAM = {
-    logPage:"Project:WAM Log"
+    logPage: 'Project:WAM Log'
 };
 
-window.UserTagsJS = {
-    modules: {},
-    tags: {
-        discordfounder: { u:'Discord Founder' },
-        discordadmin: { u:'Discord Admin' },
-        discordmod: { u:'Discord Moderator' },
-        verifiedyt: { u:'Verified YouTuber' },
-        rollback: { u:'Rollbacker' },
-        retired: { u:'Retired Staff'},
+/*
+ * UserTags configuration
+ * Reads data from [[MediaWiki:Custom-UserTags.json]]
+ * This is just so we don't need to bother with waiting
+ * for JSRT with every new update to the usergroups.
+ 
+ * @author Sidemen19
+ * @credits [[w:c:dev:MediaWiki:I18n-js/code.js]]
+ */
+
+(function ($, mw) {
+    function loadHighlights() {
+        var deferred = $.Deferred(),
+            apiEndpoint = 'https://youtube.fandom.com/api.php',
+            page = 'MediaWiki:Custom-UserTags.json',
+            params;
+
+        params = {
+            action: 'query',
+            format: 'json',
+            prop: 'revisions',
+            rvprop: 'content',
+            titles: page,
+            indexpageids: 1
+        };
+
+        mw.loader.using(['mediawiki.util'], function () {
+            $.ajax(apiEndpoint, {
+                data: params,
+                dataType: 'jsonp'
+            }).always(function (data) {
+                var res = '',
+                    revisionData = data.query && data.query.pages[data.query.pageids[0]].revisions;
+
+                if (revisionData) {
+                    res = revisionData[0]['*'];
+                }
+
+                deferred.resolve(parseMessagesToObject(res));
+            });
+        });
+
+        return deferred;
     }
-};
 
-UserTagsJS.modules.custom = {
-    'Jonpro' :['retired'],
-    'Cflm001' :['retired'],
-    'Yowuza' :['retired'],
-    'The thing' :['retired'],
-    'Flaminglawyer' :['retired'],
-    'SVG' :['retired'],
-    'TomGrove23' :['retired'],
-    'Applegirl' :['retired'],
-    'NinjaFatGuy' :['retired'],
-    'Killer365' :['retired'],
-    'Daredfox' :['retired'],
-    'Lofangas' :['retired'],
-    'Gamermadness' :['retired'],
-    'HoneyCandiez' :['retired'],
-    'Vonn Karma' :['retired'],
-    'Degrassi Fan' :['retired'],
-    'Celestial Demon' :['retired'],
-    'NKpower' :['retired'],
-    'Mickey030210navercom' :['retired'],
-    'Sims41235' :['retired'],
-    'Monochromatic Bunny' :['retired'],
-    'Wwefanboi' :['retired'],
-    'FastCube' :['retired'],
-    'Aidan Dunphy' :['retired'],
-    'Bvrrybomb' :['retired'],
-    'Ash Porcupine' :['retired'],
-    'Jvoshua': ['retired'],
-    'Flugmented': ['retired'],
-    'HanselElGato': ['discordfounder', 'retired'],
-    'FkeBld': ['discordadmin', 'verifedyt'],
-    'BoaDaSnakeMan': ['discordadmin'],
-    'Davidjl123': ['discordmod', 'verifiedyt'],
-    'JustLeafy': ['discordadmin'],
-    'DomiTheGamer': ['rollback', 'verifiedyt'],
-    'The Nameless Lancer': ['rollback'],
-    'Bendrownedfan111': ['rollback'],
-    'Yangyong08': ['rollback'],
-    'BadBlitz808': ['rollback'],
-    'Toasty Dinosaur': ['rollback', 'verifiedyt'],
-    'Sidemen19': ['discordadmin'],
-    'Cluckster': ['discordadmin'],
-    'EpicNinjaDude37': ['discordadmin', 'verifiedyt'],
-    'Zianka' :['verifiedyt'],
-    'ITurkishmapping': ['discordmod'],
-    'Popularmmos12345': ['verifiedyt'],
-    'Rickyberwick': ['verifiedyt'],
-    'IMightBeBlu': ['verifiedyt'],
-    'ZHDdude46': ['verifiedyt'],
-    'GgGibi': ['verifiedyt'],
-    'Cartoon Apocalypse': ['verifiedyt'],
-    'Official Fine Bros': ['verifiedyt'],
-    'InfiniVid': ['verifiedyt'],
-    'JackersEdit': ['verifiedyt'],
-    'Michaeltheone': ['verifiedyt'],
-    'BoosterCroc': ['verifiedyt'],
-    'Jackmasseyw': ['verifiedyt'],
-    'ActualFlyingKitty': ['verifiedyt'],
-    'SubToHarleyTBS': ['verifiedyt'],
-    'ASkylitAvenue' :['verifiedyt'],
-    'OutlookG' :['verifiedyt'],
-    'Therubinyt' :['verifiedyt'],
-    'BunnyBloYT': ['verifiedyt'],
-    'DancingFirefly': ['verifiedyt'],
-    'Prince7990': ['verifiedyt'],
-    'ItzFade': ['verifiedyt'],
-    'HarryzYT': ['verifiedyt'],
-    'ChiefJack': ['verifiedyt'],
-    'ChiefJackYT': ['verifiedyt'],
-    '99dinosaurking' : ['verifiedyt'],
-    'Ethanb0206': ['verifiedyt'],
-    'Repzion': ['verifiedyt'],
-    'TheLiving Bluejay' :['verifiedyt'],
-    'TheAwesomeGamerminer' :['verifiedyt'],
-    'Johncocek3' :['verifiedyt'],
-    'SkippoAPS' :['verifiedyt'],
-    'Snowolfpup04' :['verifiedyt'],
-    'ExoidYT' :['verifiedyt'],
-    'RainbowedYT' :['verifiedyt'],
-    'Theaidanator' :['verifiedyt'],
-    'InScane' :['verifiedyt'],
-    'Duskcoon' :['verifiedyt'],
-    'Bradey8' :['verifiedyt'],
-    'RealInkoming' :['verifiedyt'],
-    'Peppzzii' :['verifiedyt'],
-    'Disco the Hedgefox' :['verifiedyt'],
-    'DaPugFilms' :['verifiedyt'],
-    'HarmonTower805' :['verifiedyt'],
-    'Phrenomythic' :['verifiedyt'],
-    'Rugby Zone' :['verifiedyt'],
-    'NotLiink' :['verifiedyt'],
-    'Some Stupid Sketch Show Guy' :['verifiedyt'],
-    'RealClix' :['verifiedyt'],
-    'ElPran68Oficial' :['verifiedyt'],
-    'Jurta' :['verifiedyt'],
-    'Thugesh' :['verifiedyt'],
-    'Kyublitz' :['verifiedyt'],
-    'Drizzleanimations' :['verifiedyt'],
-    'ZanderLS11' :['verifiedyt'],
-    'AkaHeretic' :['verifiedyt'],
-    'Gatorbox' :['verifiedyt'],
-    'ElephantForEx' :['verifiedyt'],
-    'Famouslogos9098' :['verifiedyt'],
-    'Acharyya Bong Boy' :['verifiedyt'],
-    'VanshBhardwaj45' :['verifiedyt'],
-    'BLambClammy' :['verifiedyt'],
-    'SSBUMaster' :['verifiedyt'],
-    'Jackninja5DipperGravityFalls' :['verifiedyt'],
-    'Duier' :['verifiedyt'],
-    'Kevin08015' :['verifiedyt', 'retired'],
-    'SubscriberWars' :['verifiedyt'],
-    'Dufji' :['verifiedyt'],
-    'Plush Posse' :['verifiedyt'],
-    'EagleBuilder' :['verifiedyt'],
-    'GoldenSpyro' :['verifiedyt'],
-    'Mattthew0005' :['verifiedyt'],
-    'Convexted' :['verifiedyt'],
-    'BillehBawb' :['verifiedyt'],
-    'Tamiltechguruji' :['verifiedyt'],
-    'JulieoftheArts' :['verifiedyt'],
-    'AquaTheYoshi' :['verifiedyt'],
-    'Squidmcduck' :['verifiedyt'],
-    'SnowyPackel' :['verifiedyt'],
-    'TheRealRevzz' :['verifiedyt'],
-    'Kairossbests' :['verifiedyt'],
-    'Gabriel Jaggernauth' :['verifiedyt'],
-    'KhaosYT' :['verifiedyt'],
-    'Mtnbros' :['verifiedyt'],
-    'Bong Baccha' :['verifiedyt'],
-    'IanTEB' :['verifiedyt'],
-    'Wikiquki1' :['verifiedyt'],
-    'LukeMaster06' :['verifiedyt'],
-    'NerdieBirdieYT' :['verifiedyt'],
-    'Patonrobertz' :['verifiedyt'],
-    'Bigicedog' :['verifiedyt'],
-    'Borismartinez0' :['verifiedyt'],
-    'Frozen.bus.channel' :['verifiedyt'],
-    'DleckYT' :['verifiedyt'],
-    'SoraSkyAtic' :['verifiedyt'],
-    'FraserIRL' :['verifiedyt'],
-    'ReyGGTV' :['verifiedyt'],
-    'Theme Park Tom' :['verifiedyt'],
-    'ChrisTDL' :['verifiedyt'],
-    'NickSaysHenlo' :['verifiedyt'],
-    'ValourianBen' :['verifiedyt'],
-    'Robp98iom' :['verifiedyt'],
-    'DiamondRules01' :['verifiedyt'],
-    'Statistics and Data' :['verifiedyt'],
-    'JoPo' :['verifiedyt'],
-    'Kaiablackofficial' :['verifiedyt'],
-    'BaconBanditBoy' :['verifiedyt'],
-    'Easiorigami' :['verifiedyt'],
-    'Zer0sVoid' :['verifiedyt'],
-    'BFDIBebble': ['verifiedyt'],
-    'DandereJordan': ['verifiedyt'],
-    'Cookie95YT': ['verifiedyt'],
-    'SparkySSBU1120':['verifiedyt'],
-    'LDoubleJ': ['verifiedyt'],
-    'T3Trends': ['verifiedyt'],
-    'MikachuXD': ['verifiedyt'],
-    'Linkmon99': ['verifiedyt'],
-    '77Pacer' :['verifiedyt'],
-    'Chris6d' :['verifiedyt'],
-    '7zs' :['verifiedyt'],
-    'Kiodiego' :['verifiedyt'],
-    'Timmylogoyt' :['verifiedyt'],
-    'Javier10223' :['verifiedyt'],
-    'Katzotic1' :['verifiedyt'],
-    'Meipon' :['verifiedyt'],
-    'JoyConJames' :['verifiedyt'],
-    'Aidanlockrow' :['verifiedyt'],
-};
+    function parseMessagesToObject(res) {
+        var json = {};
+
+        try {
+            res = stripComments(res);
+            json = JSON.parse(res);
+        } catch (err) {}
+
+        return json;
+    }
+
+    function stripComments(json) {
+        json = json
+            .trim()
+            .replace(/\/\*[\s\S]*?\*\//g, '');
+
+        return json;
+    }
+
+    loadHighlights().then(function (body) {
+        UserTagsJS.modules.custom = body;
+    });
+})(jQuery, mediaWiki);
